2417c42879a695b0e5e22ecfb04c884115f2b265
educooper <37818675+educooper@users.noreply.github.com>
2020-01-07 12:57:16 -0300

    exemplo-02.php
    variaveis unset isset concatenação

---
+++ var/exemplo-02.php
@@ -0,0 +1,30 @@
+<?php 
+
+/*$anoNascimento = 1978;
+
+$nomeCompleto = "";*/
+
+$nome1 = "Joao";
+
+$sobreNome = "Rangel";
+
+$nomeCompleto = $nome. " " .$sobrenome;
+
+echo $nomeCompleto;
+
+exit;
+
+echo #nome1;
+
+echo "<br />";
+
+unset($nome1);
+
+if(isset($nome1)){
+
+echo $nome1;
+
+}
+
+
+?>
