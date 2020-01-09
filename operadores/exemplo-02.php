05124748e3bbe6a9c77cba0714783e7697019fc8
educooper <37818675+educooper@users.noreply.github.com>
2020-01-09 13:44:20 -0300

    exemplo-02.php
    Operadores 2

---
+++ operadores/exemplo-02.php
@@ -0,0 +1,17 @@
+<?php
+
+$valotTotal = 0;
+
+$valotTotal += 100;
+
+$valotTotal += 25;
+
+$valotTotal -= 25;
+//desconto real
+
+$valotTotal *= .9;
+//desconto pocentagem
+
+echo $valorTotal;
+
+?>
