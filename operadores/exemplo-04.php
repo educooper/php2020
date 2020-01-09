8984fad662df611cda86ed60f4c43ca4c5ba8c57
educooper <37818675+educooper@users.noreply.github.com>
2020-01-09 13:57:21 -0300

    Create exemplo-04.php
    Operador de Comparação.

---
+++ operadores/exemplo-04.php
@@ -0,0 +1,35 @@
+<?php
+
+//$a = 30;
+$a = 55.0;
+$b = 55;
+
+var_dump($a > $b);
+echo"<br />";
+
+var_dump($a < $b);
+echo"<br />";
+
+var_dump($a == $b);
+// =  operador de atribuição mas para comparação usa-se ==
+echo"<br />";
+
+var_dump($a === $b);
+// Operador de igualdade e identidade - mesmo valor e mesmo tipo de dados
+echo"<br />";
+
+var_dump($a != $b);
+//São diferentes no valor
+echo"<br />";
+
+var_dump($a !== $b);
+//São diferentes no valor e no tipo
+echo"<br />";
+
+var_dump($a > $b);
+echo"<br />";
+
+var_dump($a > $b);
+echo"<br />";
+
+?>
