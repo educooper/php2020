9a0b3511c915a835ed4f309523b4371aa4273462
educooper <37818675+educooper@users.noreply.github.com>
2020-01-09 13:50:10 -0300

    exemplo-03.php
    Operadores Aritmédicos

---
+++ operadores/exemplo-03.php
@@ -0,0 +1,26 @@
+<?php
+/*17. Operadores aritmédicos*/
+
+$a = 10;
+$b = 2;
+
+echo = $a + $b;
+echo "<br />";
+
+echo = $a - $b;
+echo "<br />";
+
+echo = $a * $b;
+echo "<br />";
+
+echo = $a / $b;
+echo "<br />";
+
+echo = $a % $b;
+echo "<br />";
+
+echo = $a == $b;
+echo "<br />";
+
+
+?>
