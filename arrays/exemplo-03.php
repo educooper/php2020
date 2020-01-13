7a00149e8653e4b079d3212502f2a8522148056c
educooper <37818675+educooper@users.noreply.github.com>
2020-01-13 13:05:48 -0300

    Create exemplo-03.php
    arrays bidimencionais

---
+++ arrays/exemplo-03.php
@@ -0,0 +1,19 @@
+<?php
+//25 ARRAYS
+//Array bidimensional
+
+
+$pessoas = array();
+
+array_push($pessoas,array(
+'nome'=>'João',
+'idade'=>20
+));
+
+array_push($pessoas,array(
+'nome'=>'Glaucio',
+'idade'=>25
+));
+
+print_r($pessoas);
+?>
