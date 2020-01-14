2092b3e78c559bc060be17a9077072668d4ba21b
educooper <37818675+educooper@users.noreply.github.com>
2020-01-14 13:03:15 -0300

    Create exemplo-02.php

---
+++ constantes/exemplo-02.php
@@ -0,0 +1,20 @@
+<?php
+
+//27. CONSTANTES
+
+define("BANCO_DE_DADOS",
+['127.0.0.1', 
+//endereço
+'root', 
+//user
+'password', 
+//senha
+'test',]); 
+//nome do banco de dados
+/*Se quiser que os parametros sehjam CASEINSENSITIVE, ou seja, rode independente da caixa alta ou baixa, se usa o parametro true apos chave
+,]true);
+*/
+print_r(BANCO_DE_DADOS);
+
+
+?>
