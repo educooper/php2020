6c148f0f1383f903c6a60d55f693b3ae0d725f89
educooper <37818675+educooper@users.noreply.github.com>
2020-01-14 12:51:30 -0300

    Update exemplo-02.php

--- JSON/exemplo-02.php
+++ JSON/exemplo-02.php
@@ -5,7 +5,7 @@
 
 $json = '[{"nome":"Jo\u003eo","idade":20},{"nome":"Glaucio","idade":25}]';
 
-$data = $json_decode($json,true);
+$data = $json_decode($json,true); //Sempre usar true senão vira object
 
 var_dump($data);
 
