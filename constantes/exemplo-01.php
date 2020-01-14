5dd5f3828f775b12eaeb90b0c4b7cdbaa3e798c4
educooper <37818675+educooper@users.noreply.github.com>
2020-01-14 12:55:39 -0300

    Create exemplo-01.php

---
+++ constantes/exemplo-01.php
@@ -0,0 +1,10 @@
+<?php
+
+//27. CONSTANTES e arrays constantes
+// Se usa sempre letras maiusculas o que é uma convenção. Rececve 2 parametros nome e valor.
+
+define("sERVIDOR","127.0.0.1");
+
+eco SERVIDOR;
+
+?>
