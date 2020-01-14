3cd0c5d5d223ff328c346822ef4ae6d1341a861c
educooper <37818675+educooper@users.noreply.github.com>
2020-01-14 13:52:12 -0300

    Update exemplo-02.php

--- sessoes/exemplo-02.php
+++ sessoes/exemplo-02.php
@@ -3,10 +3,17 @@
 //28. SESSÕES
 
 session_start();
+session_unset($_SESSION['nome']);
+//limpa a sessão especifica do nome ou ($_SESSION) para limpar todas as sessões ativas
 
 echo $_SESSION['nome'];
 
 //Criação de Sessão  - testar Exemplo 02 e depois exemplo 01.
 //Lembre-se arquivos de configuração .inc não são seguros. 
+//Para boas praticas se usa confi ou configurator.php com as regras de sesson start bem como outras diretivas 
+
+session_destroy();
+//limpa sessão da memória remove usuário da sessão atual
+
 
 ?>
