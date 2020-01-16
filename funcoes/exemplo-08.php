a21faa8df9faed962db7ab3f2e39c2ab1bd2634c
educooper <37818675+educooper@users.noreply.github.com>
2020-01-16 14:10:04 -0300

    Create exemplo-08.php

---
+++ funcoes/exemplo-08.php
@@ -0,0 +1,18 @@
+<?php
+
+//34. NOVIDADES EM FUNÇÕES EM PHP7
+
+  function soma(int ...$valores):string{ //pega todos os parametros de um array de uma vez só
+  // DECLARAÇÃO DE TIPOS DE DADOS  : float retorna todo sem ponto flutuante
+      
+      return array_sum($valores); // arrray especial que soma os parametros
+  
+  }
+  
+echo soma(2, 2);
+  echp "<br />";
+echo soma(25, 33);
+  echp "<br />";
+echo soma(1.5, 3.2);
+  echp "<br />";
+?>
