04fd64e3aec7263641c3401f87be96a77d5bdd33
educooper <37818675+educooper@users.noreply.github.com>
2020-01-17 13:11:13 -0300

    Create exemplo-01.php

---
+++ funcoes/date/exemplo-01.php
@@ -0,0 +1,16 @@
+<?php
+
+//37. FUNÇÃO DATE E TIME
+/*
+
+Não é sensível ao setlocale, isto é, não identifica automaticamente fuso horário, a  não ser timestamp do servidor
+*/
+echo date("d/m/Y H:i:s", 148659982121); //timestamp
+
+echo "<br />";
+
+echo time();
+
+//teste - coloque o resultado na função date.
+
+?>
