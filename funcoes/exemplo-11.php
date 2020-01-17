0f26321cbf1d9a22d28f85a21cac3008b35332bd
educooper <37818675+educooper@users.noreply.github.com>
2020-01-17 13:02:50 -0300

    Create exemplo-11.php

---
+++ funcoes/exemplo-11.php
@@ -0,0 +1,23 @@
+<?php
+
+35. FUNCÕES ANÔNIMAS
+
+/*
+
+Quando ocorrem as funções anônimas: 
+
+Passar função como parametro
+
+Atribuir numa função em uma variavel
+
+*/
+
+$fn = function($a){
+
+    var_dump($a);
+
+};
+
+$fn("Oi");
+
+?>
