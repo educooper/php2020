5a5c1c51f862a382e456a09bf2f9d5429ecb5fba
educooper <37818675+educooper@users.noreply.github.com>
2020-01-24 12:20:38 -0300

    Create exemplo-02.php

---
+++ pdo/exemplo-02.php
@@ -0,0 +1,25 @@
+<?php
+/*
+COMANDOS NO MICROSOFT SERVER EXPRESS
+
+CREATE TABLE tb_usuarios (
+idusuario NOT NULL IDENTIFY PRIMARY KEY,
+deslogin VARCHAR(64) NOT NULL,
+dessenha VARCHAR(256) NOT NULL,
+dtcadastro DATETIME NOT NULL DEFAULT GETDATE()
+);
+
+
+SELECT * FROM tb_usuarios;
+
+INSERT INTO tb_usuarios (deslogin,dessenha) VALUES ('user',  '!@#$');
+
+
+IMPORTANTE - ADICIONAR DLLS SQL SERVER NO PHP.INI depois da linha extensios l.846
+
+*/
+
+
+
+
+?>
