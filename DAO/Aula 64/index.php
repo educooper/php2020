009fe43008f2bead7fb54164aaad2d90baa2ff60
educooper <37818675+educooper@users.noreply.github.com>
2020-02-05 14:05:56 -0300

    Update index.php

--- DAO/Aula 64/index.php
+++ DAO/Aula 64/index.php
@@ -18,21 +18,4 @@ echo json_encode($lista);
 
 //echo json_encode($search);
 
-
-//carrega um usuario usando um login e uma senha
-
-//$usuario = new Usuario();
-
-//$usuario->login("vizinha", "69");
-
-//echo $usuario;
-
-
-$aluno = new Usuario("aluno", "@lun0");
-
-$aluno->insert();
-
-echo $aluno;
-
-
 ?>
