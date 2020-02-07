041ea73ef01804e64db2eaba66e7951b8fd88878
educooper <37818675+educooper@users.noreply.github.com>
2020-02-07 13:58:12 -0300

    Update index.php

--- DAO/index.php
+++ DAO/index.php
@@ -1,10 +1,17 @@
 <?php
 
 require_once("config.php");
+//62. DAO
+$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+echo json_encode($usuarios);
+
 // Carrega um usuario
-$root = new Usuario();
-$root->loadbyId(26);
-echo $root;
+//$root = new Usuario();
+//$root->loadbyId(26);
+//echo $root;
 
 //Carrega uma lsita de usuarios
 //
