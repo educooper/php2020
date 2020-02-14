03e0efdb4be8d197503a13deb5fc3d2eecc15e6d
Edu Cooper <edusardo@uninove.edu.br>
2020-02-14 06:39:04 -0300

    ok

--- DIR/exemplo-02.php
+++ DIR/exemplo-02.php
@@ -1,25 +1,27 @@
 <?php
 //68. Lendo e manipulando diretorios e arquivos no php7
-$images = candir("images");
-//Observação - carregar iamgens na pasta criada.
+$images = scandir("images");
 //var_dump($images);
 
 $data = array();
 
-foreach (in_array($img, array(".", ".."))) {
+foreach($images as $img){ 
+if (!in_array($img, array(".", ".."))) {
 	
-	$filename = "" . DIRECTORT_SEPARATOR . $img;
+	$filename = "images" . DIRECTORY_SEPARATOR . $img;
 	
 	$info = pathinfo($filename);
 	
 	$info["size"] = filesize($filename);
 	$info["modifield"] = date("d/m/Y H:i:s", filemtime($filename));
-	$info["url"] = "http://127.0.0.1/php-2020/DIR/".str_replace("\\", "/", $filename);
+	$info["url"] = "http://127.0.0.1/udemy-20/DIR/".str_replace("\\", "/", $filename);
 	
-	array_push($info,$sata);
+	array_push($data,$info);
 	
-	}	
-	
+	}
+
 }
+
+echo json_encode($data);
 	
 ?>
