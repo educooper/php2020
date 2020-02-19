9c71cd030fc11856e6d901135611bd617fd2fcf2
educooper <37818675+educooper@users.noreply.github.com>
2020-02-19 13:46:35 -0300

    Create exemplo-01.php
    ok

---
+++ curl/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php
+$cep = "";
+
+$link = "https://viacep.com.br/ws/$cep/json/"
+
+$ch = curl_init($link);
+
+curl_setopr($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopr($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+$data = json_decode($response, true);
+
+print_r($data);
+
+?>
