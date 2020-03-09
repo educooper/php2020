48b47dbdde18eb56b27b7f91bb9fc7c3e2c47a40
educooper <37818675+educooper@users.noreply.github.com>
2020-03-09 14:05:25 -0300

    Update index.php

--- tpl/index.php
+++ tpl/index.php
@@ -2,4 +2,21 @@
 
 require_once("vendor/autoload.php");
 
+use Rain\Tpl;
+
+$config = array(
+
+"tpl_dir"    =>  "tpl/",
+"cache_dir"  =>  "cache/"  
+);
+
+Tpl::configure( $config);
+
+$tpl = new Tpl;
+
+$tpl->assign( "name", "HCode" );
+$tpl->assign( "version " PHP_VERSION);
+
+$tpl->draw("index");
+
 ?>
